7588c69505b0d960ded2f2960fc828b89b153829
freewind <nowindlee@gmail.com>
2018-09-03 11:23:41 +0800

    demo

--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,7 +1,21 @@
 package example
 
+import java.io.File
+import java.util.concurrent.TimeUnit
+
 fun main(args: Array<String>) {
-    println(hello("Kotlin"))
+    val output = runCommand(File("."), "./gradlew compileKotlin")
+    println(output)
 }
 
-fun hello(name: String): String = "Hello, $name!"
+private fun runCommand(workingDir: File, command: String): String {
+    val parts = command.split("\\s+".toRegex())
+    val proc = ProcessBuilder(*parts.toTypedArray())
+            .directory(workingDir)
+            .redirectOutput(ProcessBuilder.Redirect.PIPE)
+            .redirectError(ProcessBuilder.Redirect.PIPE)
+            .start()
+
+    proc.waitFor(60, TimeUnit.MINUTES)
+    return proc.inputStream.bufferedReader().readText()
+}
